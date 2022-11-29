87ea383e479ef818681fd818b8d2140e64452285
roshani-patel <98912110+roshani-patel@users.noreply.github.com>
2022-11-29 20:23:17 +0530

    Add files via upload

---
+++ src/context/github/githubContext.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+const githubContext = createContext();
+
+export default githubContext;
+
+//Creating the context for universal store
+//createContext
+//name of context githubContext
